15533fec76a97389f97af62d8578c1db04bf97e6
PythonXHunter <aryan.computerscience@gmail.com>
2024-09-20 23:12:34 +0530

    navbar completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import NavBar from "./components/NavBar/NavBar";
 
 export default function App(){
   return (
-    <div>
+    <>
       <NavBar />
-    </div>
+    </>
   )
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,28 +1,37 @@
+import { useState } from 'react';
+import './NavBar.css'
+
 export default function NavBar(){
+  const [toggle, setToggle] = useState(false);
   const linkList = ['About Me', 'Experience', 'Education', 'Projects', 'Achievements', 'Contact', 'Blog', 'Gallery']
   return (
     <header>
-      <nav>
-        <div className="navbar">
-          <a>
-            <img src="/icons/fav.png" />
-            Hugo Profile
-          </a>
-          <input />
-          <div>
-            <ul>
+      <nav className='navbar'>
+        <div className='navbar-container'>
+        <a className="navbar-brand" href="/" style={{ display: 'inline-block' }}>
+          <img src="/icons/fav.png" style={{ width: '30px', height: '30px', verticalAlign: 'middle' }} />
+          <span style={{ verticalAlign: 'middle' }}> Hugo Profile</span>
+        </a>
+          <input 
+            id="search"
+            placeholder='Ctrl + k to Search...'
+          />
+          <div id='navbar-content'>
+            <ul className='navbar-nav'>
               {
-                linkList.map((item, index) => 
-                <li key={index}>
-                  {item}
-                </li>
-              )}
+                linkList.map(link => (
+                  <li key={link} className='navbar-item'>
+                    <a className='navbar-link'>{link}</a>
+                  </li>
+                ))
+              }
+              <li className='navbar-item'>
+                <button id='theme-toggle' onClick={() => setToggle(!toggle)} >
+                  <img src={`/icons/${toggle ? 'moon' : 'sun'}.svg`} />
+                </button>
+              </li>
             </ul>
           </div>
-          <svg id="moon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentcolor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-            <path d="M21 12.79A9 9 0 1111.21 3 7 7 0 0021 12.79z">
-            </path>
-          </svg>
         </div>
       </nav>
     </header>
