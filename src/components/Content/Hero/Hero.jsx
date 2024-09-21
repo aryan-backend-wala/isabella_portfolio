2f4bb507e36046e3f2d80b16da5f9f00885a66c6
PythonXHunter <aryan.computerscience@gmail.com>
2024-09-21 18:20:37 +0530

    dark mode added to hero

--- src/components/Content/Hero/Hero.jsx
+++ src/components/Content/Hero/Hero.jsx
@@ -2,38 +2,43 @@ import './Hero.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faXTwitter } from '@fortawesome/free-brands-svg-icons';
+import { useContext } from 'react';
+import { ThemeContext } from '../../../ThemeContext';
+
 export default function Hero() {
+  const { isDarkMode } = useContext(ThemeContext);
+
   return <section id="hero">
     <div className='hero-container'>
       <div className='row' style={{ height: "390px" }}>
         <div className='column'>
-          <span className='subtitle'>Hi, my name is</span>
-          <h2>Isabella.</h2>
-          <h3>I build things for the web</h3>
-          <p className='hero-content'>
+          <span className={`subtitle ${isDarkMode? 'dark' : 'light'}`}>Hi, my name is</span>
+          <h2 className={isDarkMode? 'dark' : 'light'}>Isabella.</h2>
+          <h3 className={isDarkMode? 'dark' : 'light'}>I build things for the web</h3>
+          <p className={`hero-content ${isDarkMode? 'dark' : 'light'}`}>
             A passionate web app developer. I tend to make use of modern web technologies to build
             websites that look great, feel fantastic, and function correctly.
           </p>
           <div className='row' style={{ height: '98px' }}>
             <div className='left-content'>
-              <a href='#' className='btn' download>
+              <a href='#' className={`btn ${isDarkMode? 'dark' : 'light'}`} download>
                 Resume
               </a>
             </div>
             <div className='right-content'>
               <span>
                 <span className='px-1'>
-                  <a href="https://example.com" target="_blank" class="btn social-icon">
+                  <a href="https://example.com" target="_blank" className={`btn social-icon ${isDarkMode? 'dark' : 'light'}`}>
                     <FontAwesomeIcon icon={faGithub}  className='fa-icons'/>
                   </a>
                 </span>
                 <span className='px-1'>
-                  <a href="https://example.com" target="_blank" class="btn social-icon">
+                  <a href="https://example.com" target="_blank" className={`btn social-icon ${isDarkMode? 'dark' : 'light'}`}>
                     <FontAwesomeIcon icon={faXTwitter} className='fa-icons' />
                   </a>
                 </span>
                 <span className='px-1'>
-                  <a href="https://example.com" target="_blank" class="btn social-icon">
+                  <a href="https://example.com" target="_blank" className={`btn social-icon ${isDarkMode? 'dark' : 'light'}`}>
                     <img src='/icons/fav.png' style={{height: "16px"}} />
                   </a>
                 </span>
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -31,7 +31,7 @@ export default function NavBar() {
                   {
                     isDarkMode ?
                       <svg id="sun"
-                        xmlns="http://www.w3.org/2000/svg" width="24" height="18" viewBox="0 0 24 24" fill="none" stroke="#e4e6eb" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        xmlns="http://www.w3.org/2000/svg" width="24" height="18" viewBox="0 0 24 24" fill="none" stroke="#e4e6eb" strokeWidth="2" strokeLinecap="round">
                         <circle cx="12" cy="12" r="5"></circle>
                         <line x1="12" y1="1" x2="12" y2="3"></line>
                         <line x1="12" y1="21" x2="12" y2="23"></line>
@@ -43,7 +43,7 @@ export default function NavBar() {
                         <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
                       </svg> :
                       <svg id="moon"
-                        xmlns="http://www.w3.org/2000/svg" width="24" height="18" viewBox="0 0 24 24" fill="none" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        xmlns="http://www.w3.org/2000/svg" width="24" height="18" viewBox="0 0 24 24" fill="none" stroke="black" strokeWidth="2" strokeLinecap="round">
                         <path d="M21 12.79A9 9 0 1111.21 3 7 7 0 0021 12.79z">
                         </path>
                       </svg>
