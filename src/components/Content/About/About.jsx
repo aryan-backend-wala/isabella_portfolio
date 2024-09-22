62b1b7fb9176d01dd1d49ece89b4abd13f010575
PythonXHunter <aryan.computerscience@gmail.com>
2024-09-22 14:22:40 +0530

    dark mode completed

--- src/components/Content/About/About.jsx
+++ src/components/Content/About/About.jsx
@@ -7,15 +7,15 @@ export default function About() {
   const listItems = ['HTML', 'CSS', 'JavaScript', 'React', 'Node', 'Express']
   return <section id="about-me">
     <div className='about-container'>
-      <h3 className={`h3 ${isDarkMode ? 'dark' : 'light'}`}>About Me</h3>
+      <h3 style={{color: isDarkMode ? 'rgb(176, 179, 184)' : 'rgb(108, 117, 125)'}}>About Me</h3>
       <div className='about-row' style={{height: '405px'}}>
         <div className='about-image' style={{ padding: '0 12px' }}>
           <img src='/icons/me.png' style={{ height: '309px', padding: '0 48px' }} />
         </div>
-        <div style={{ padding: '0 12px'}} className='content'>
+        <div style={{ padding: '0 12px', color: isDarkMode ? 'rgb(228, 230, 235)' : 'rgb(52, 58, 64)'}} className='content'>
           <p>
             I am a software developer with a passion for web development. I have a background in computer science and mathematics, and I have a strong interest in the intersection of technology and art. I am currently working as a software developer at
-            <a href='https://example.com'>
+            <a href='https://example.com' style={{color: isDarkMode ? 'rgb(228, 230, 235)' : '#007bff'}}>
               &nbsp;example org.&nbsp;
             </a>
             in San Francisco, CA.
@@ -27,7 +27,7 @@ export default function About() {
           <ul>
             {
               listItems.map(skill => (
-                <li>{skill}</li>
+                <li className={isDarkMode ? 'dark' : 'light'}>{skill}</li>
               ))
             }
           </ul>
